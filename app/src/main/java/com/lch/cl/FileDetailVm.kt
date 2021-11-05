fcdc86b2856234816e02a7eb909942f194a0fd0e
lichenghang <lichenghang@meetstarinc.com>
2021-11-05 12:21:59 +0800

    fix

--- app/src/main/java/com/lch/cl/FileDetailVm.kt
+++ app/src/main/java/com/lch/cl/FileDetailVm.kt
@@ -1,12 +1,18 @@
 package com.lch.cl
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
-class FileDetailVm:BaseVm() {
-    val state=MutableLiveData<FileDetailUiState>()
+class FileDetailVm : BaseVm() {
+    val state = MutableLiveData<FileDetailUiState>()
 
-    fun load(path:String?){
-
+    fun load(path: String?) {
+        viewModelScope.launch {
+            if (path != null) {
+                state.value = FileDetailUiState(path, this@FileDetailVm)
+            }
+        }
     }
 
 }
