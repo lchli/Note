58c38f9bf81dcab661614f2c1b37b1a1879552c8
lichenghang <lichenghang@babytree-inc.com>
2022-04-21 02:01:43 +0800

    aop

--- app/src/main/java/com/lch/AppMethodBeat.java
+++ app/src/main/java/com/lch/AppMethodBeat.java
@@ -1,12 +1,44 @@
 package com.lch;
 
+import android.os.Looper;
+import android.util.Log;
+
 public class AppMethodBeat
 {
+   static long st=-1;
+
     public static void i(int methodId) {
+        if(Looper.myLooper()!=Looper.getMainLooper()){
+            return;
+        }
+        st=System.currentTimeMillis();
 
     }
 
-    public static void o(int methodId) {
+    public static void o(String classname,String methodname,String desc) {
+        if(Looper.myLooper()!=Looper.getMainLooper()){
+            return;
+        }
+        if(st<0){
+            return;
+        }
+        long spend=System.currentTimeMillis()-st;
+        st=-1;
+
+        if(spend<50){
+            return;
+        }
+        StringBuilder sb=new StringBuilder();
+        sb.append("slow method:")
+                .append(spend)
+                .append(",")
+                .append(classname)
+                .append("-")
+                .append(methodname)
+                .append("-")
+                .append(desc);
+
+        Log.e("sss",sb.toString());
 
     }
 }
--- app/src/main/java/com/lch/MyAopVisitor.java
+++ app/src/main/java/com/lch/MyAopVisitor.java
@@ -6,39 +6,54 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.commons.AdviceAdapter;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class MyAopVisitor {
+    private final List<String> collectedIgnoreMethod = new ArrayList<>();
 
     public byte[] modifyClassBytes(byte[] source) {
         ClassReader classReader = new ClassReader(source);
         ClassWriter classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
-        ClassVisitor classVisitor = new TraceClassAdapter(Opcodes.ASM5, classWriter);
+        ClassVisitor classVisitor = new FindMethodInfoVisitor(Opcodes.ASM5, classWriter);
+        classReader.accept(classVisitor, ClassReader.EXPAND_FRAMES);
+       // source = classWriter.toByteArray();
+
+        System.err.println("collectedIgnoreMethod:"+collectedIgnoreMethod);
+
+
+        classReader = new ClassReader(source);
+        classWriter = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        classVisitor = new TraceClassAdapter(Opcodes.ASM5, classWriter);
         classReader.accept(classVisitor, ClassReader.EXPAND_FRAMES);
         return classWriter.toByteArray();
     }
 
-    private class TraceClassAdapter extends ClassVisitor {
+    private static boolean isIgnoreClass(String classname) {
 
+        return classname.contains("AppMethodBeat");
+    }
+
+    private class FindMethodInfoVisitor extends ClassVisitor {
         private String className;
         private String superName;
         private boolean isABSClass = false;
-        private boolean hasWindowFocusMethod = false;
-        private boolean isActivityOrSubClass;
-        private boolean isNeedTrace = true;
 
-        TraceClassAdapter(int i, ClassVisitor classVisitor) {
+
+        FindMethodInfoVisitor(int i, ClassVisitor classVisitor) {
             super(i, classVisitor);
         }
+
 
         @Override
         public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
             super.visit(version, access, name, signature, superName, interfaces);
             this.className = name;
             this.superName = superName;
-            // this.isActivityOrSubClass = isActivityOrSubClass(className, collectedClassExtendMap);
-            // this.isNeedTrace = MethodCollector.isNeedTrace(configuration, className, mappingCollector);
             if ((access & Opcodes.ACC_ABSTRACT) > 0 || (access & Opcodes.ACC_INTERFACE) > 0) {
                 this.isABSClass = true;
             }
@@ -49,68 +64,186 @@ public class MyAopVisitor {
         public MethodVisitor visitMethod(int access, String name, String desc,
                                          String signature, String[] exceptions) {
 
+            if (isIgnoreClass(className)) {
+                return super.visitMethod(access, name, desc, signature, exceptions);
+            }
             if (isABSClass) {
                 return super.visitMethod(access, name, desc, signature, exceptions);
-            } else {
-                MethodVisitor methodVisitor = cv.visitMethod(access, name, desc, signature, exceptions);
-                return new TraceMethodAdapter(api, methodVisitor, access, name, desc, this.className,
-                        hasWindowFocusMethod, isActivityOrSubClass, isNeedTrace);
             }
+            return new CollectMethodNode(className, access, name, desc, signature, exceptions);
+
         }
 
+
+        private class CollectMethodNode extends MethodNode {
+            private String className;
+            private boolean isConstructor;
+
+
+            CollectMethodNode(String className, int access, String name, String desc,
+                              String signature, String[] exceptions) {
+                super(Opcodes.ASM5, access, name, desc, signature, exceptions);
+                this.className = className;
+            }
+
+            @Override
+            public void visitEnd() {
+                super.visitEnd();
+
+                if ("<init>".equals(name)) {
+                    isConstructor = true;
+                }
+                // filter simple methods
+                if ((isEmptyMethod() || isGetSetMethod() || isSingleMethod())) {
+                    collectedIgnoreMethod.add(name + "-" + desc);
+                    return;
+                }
+
+            }
+
+            private boolean isGetSetMethod() {
+                int ignoreCount = 0;
+                ListIterator<AbstractInsnNode> iterator = instructions.iterator();
+                while (iterator.hasNext()) {
+                    AbstractInsnNode insnNode = iterator.next();
+                    int opcode = insnNode.getOpcode();
+                    if (-1 == opcode) {
+                        continue;
+                    }
+                    if (opcode != Opcodes.GETFIELD
+                            && opcode != Opcodes.GETSTATIC
+                            && opcode != Opcodes.H_GETFIELD
+                            && opcode != Opcodes.H_GETSTATIC
+
+                            && opcode != Opcodes.RETURN
+                            && opcode != Opcodes.ARETURN
+                            && opcode != Opcodes.DRETURN
+                            && opcode != Opcodes.FRETURN
+                            && opcode != Opcodes.LRETURN
+                            && opcode != Opcodes.IRETURN
+
+                            && opcode != Opcodes.PUTFIELD
+                            && opcode != Opcodes.PUTSTATIC
+                            && opcode != Opcodes.H_PUTFIELD
+                            && opcode != Opcodes.H_PUTSTATIC
+                            && opcode > Opcodes.SALOAD) {
+                        if (isConstructor && opcode == Opcodes.INVOKESPECIAL) {
+                            ignoreCount++;
+                            if (ignoreCount > 1) {
+                                return false;
+                            }
+                            continue;
+                        }
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            private boolean isSingleMethod() {
+                ListIterator<AbstractInsnNode> iterator = instructions.iterator();
+                while (iterator.hasNext()) {
+                    AbstractInsnNode insnNode = iterator.next();
+                    int opcode = insnNode.getOpcode();
+                    if (-1 == opcode) {
+                        continue;
+                    } else if (Opcodes.INVOKEVIRTUAL <= opcode && opcode <= Opcodes.INVOKEDYNAMIC) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+
+            private boolean isEmptyMethod() {
+                ListIterator<AbstractInsnNode> iterator = instructions.iterator();
+                while (iterator.hasNext()) {
+                    AbstractInsnNode insnNode = iterator.next();
+                    int opcode = insnNode.getOpcode();
+                    if (-1 == opcode) {
+                        continue;
+                    } else {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+        }
+    }
+
+    private class TraceClassAdapter extends ClassVisitor {
+
+        private String className;
+        private String superName;
+        private boolean isABSClass = false;
+
+        TraceClassAdapter(int i, ClassVisitor classVisitor) {
+            super(i, classVisitor);
+        }
 
         @Override
-        public void visitEnd() {
+        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+            super.visit(version, access, name, signature, superName, interfaces);
+            this.className = name;
+            this.superName = superName;
+            if ((access & Opcodes.ACC_ABSTRACT) > 0 || (access & Opcodes.ACC_INTERFACE) > 0) {
+                this.isABSClass = true;
+            }
 
-            super.visitEnd();
         }
+
+        @Override
+        public MethodVisitor visitMethod(int access, String name, String desc,
+                                         String signature, String[] exceptions) {
+
+            if (isIgnoreClass(className)) {
+                return super.visitMethod(access, name, desc, signature, exceptions);
+            }
+            if (isABSClass) {
+                return super.visitMethod(access, name, desc, signature, exceptions);
+            }
+
+
+            MethodVisitor methodVisitor = cv.visitMethod(access, name, desc, signature, exceptions);
+            return new TraceMethodAdapter(api, methodVisitor, access, name, desc,className);
+
+        }
+
+
     }
 
     private class TraceMethodAdapter extends AdviceAdapter {
 
-        private final String methodName;
-        private final String name;
-        private final String className;
-        private final boolean hasWindowFocusMethod;
-        private final boolean isNeedTrace;
-        private final boolean isActivityOrSubClass;
 
-        protected TraceMethodAdapter(int api, MethodVisitor mv, int access, String name, String desc, String className,
-                                     boolean hasWindowFocusMethod, boolean isActivityOrSubClass, boolean isNeedTrace) {
+        private final String classname;
+
+        protected TraceMethodAdapter(int api, MethodVisitor mv, int access, String name, String desc, String classname) {
             super(api, mv, access, name, desc);
-            //TraceMethod traceMethod = TraceMethod.create(0, access, className, name, desc);
-            this.methodName = name;// traceMethod.getMethodName();
-            this.hasWindowFocusMethod = hasWindowFocusMethod;
-            this.className = className;
-            this.name = name;
-            this.isActivityOrSubClass = isActivityOrSubClass;
-            this.isNeedTrace = isNeedTrace;
+            this.classname=classname;
 
         }
 
         @Override
         protected void onMethodEnter() {
-            // TraceMethod traceMethod = collectedMethodMap.get(methodName);
-            // if (traceMethod != null) {
-            // traceMethodCount.incrementAndGet();
+            if (collectedIgnoreMethod.contains(getName() + "-" + methodDesc)) {
+                return;
+            }
             mv.visitLdcInsn(0);
             mv.visitMethodInsn(INVOKESTATIC, TraceBuildConstants.MATRIX_TRACE_CLASS, "i", "(I)V", false);
-
-//                if (checkNeedTraceWindowFocusChangeMethod(traceMethod)) {
-//                    traceWindowFocusChangeMethod(mv, className);
-//                }
-            //  }
         }
 
 
         @Override
         protected void onMethodExit(int opcode) {
-            // TraceMethod traceMethod = collectedMethodMap.get(methodName);
-            //if (traceMethod != null) {
-            // traceMethodCount.incrementAndGet();
-            mv.visitLdcInsn(0);
-            mv.visitMethodInsn(INVOKESTATIC, TraceBuildConstants.MATRIX_TRACE_CLASS, "o", "(I)V", false);
-            // }
+            if (collectedIgnoreMethod.contains(getName() + "-" + methodDesc)) {
+                return;
+            }
+
+            mv.visitLdcInsn(classname);
+            mv.visitLdcInsn(getName());
+            mv.visitLdcInsn(methodDesc);
+            mv.visitMethodInsn(INVOKESTATIC, TraceBuildConstants.MATRIX_TRACE_CLASS, "o", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", false);
         }
 
 
